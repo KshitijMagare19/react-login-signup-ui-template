3f3642947d5f09447538cc2e9e51aa8ced5cf4ef
KshitijMagare19 <kshitij.magare19@vit.edu>
2023-06-19 12:13:02 +0530

    signup log

--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react'
 
 export default class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fname: "",
+      lname: "",
+      email: "",
+      password: "",
+    };
+    this.handleSumbit = this.handleSumbit.bind(this);
+  }
+  handleSumbit(e) {
+    e.preventDefault();
+    const {fname, lname, email, password} = this.state;
+    console.log(fname, lname, email, password);
+  }
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSumbit}>
         <h3>Sign Up</h3>
 
         <div className="mb-3">
@@ -12,12 +27,13 @@ export default class SignUp extends Component {
             type="text"
             className="form-control"
             placeholder="First name"
+            onChange={(e) => this.setState({ fname: e.target.value })}
           />
         </div>
 
         <div className="mb-3">
           <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" />
+          <input type="text" className="form-control" placeholder="Last name" onChange={(e) => this.setState({ lname: e.target.value })} />
         </div>
 
         <div className="mb-3">
@@ -26,6 +42,7 @@ export default class SignUp extends Component {
             type="email"
             className="form-control"
             placeholder="Enter email"
+            onChange={(e) => this.setState({ email: e.target.value })}
           />
         </div>
 
@@ -35,6 +52,7 @@ export default class SignUp extends Component {
             type="password"
             className="form-control"
             placeholder="Enter password"
+            onChange={(e) => this.setState({ password: e.target.value })}
           />
         </div>
 
